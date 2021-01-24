ee2368ac5c6ba1a5f809c15a1262b63d7d530b16
Milinda Senaka <milindasenaka.1@gmail.com>
2021-01-24 18:50:45 +0530

    display alert box

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import {
   View,
   FlatList,
   Platform,
-  StatusBar
+  StatusBar,
+  Alert
 } from "react-native";
 import Header from './components/header';
 import TodoItem from './components/todoitem';
@@ -25,12 +26,20 @@ export default function App() {
   }
 
   const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [
-        { text: text, key: Math.random().toString() },
-        ...prevTodos
-      ]
-    })
+
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos
+        ]
+      });
+    } else {
+      Alert.alert('oops!', 'Todo must be over 3 chars long', [
+        { text: 'Understood', onPress: () => console.log('Alert Closed') }
+      ]);
+    }
+
   }
   return (
     <View style={styles.container}>
